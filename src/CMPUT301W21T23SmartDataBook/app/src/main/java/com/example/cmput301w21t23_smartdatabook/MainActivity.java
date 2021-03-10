4b1d3b595d5586fb4b2dfd5c44738842e0533d7b
NatDev7 <56232333+NatDev7@users.noreply.github.com>
2021-03-09 20:28:09 -0700

    Created experiment_details xml files

--- src/CMPUT301W21T23SmartDataBook/app/src/main/java/com/example/cmput301w21t23_smartdatabook/MainActivity.java
+++ src/CMPUT301W21T23SmartDataBook/app/src/main/java/com/example/cmput301w21t23_smartdatabook/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.followed_experiments);
+        setContentView(R.layout.edit_profile);
     }
 }
