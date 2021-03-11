70ba4c58ad9a05478041e4c6cdf575dd9864f036
Afaq <63483862+AfaqNabi@users.noreply.github.com>
2021-03-10 20:35:23 -0700

    bottom tabs
    added bottom tabs functionality but does not fully work yet

--- src/CMPUT301W21T23SmartDataBook/app/src/main/java/com/example/cmput301w21t23_smartdatabook/favPage.java
+++ src/CMPUT301W21T23SmartDataBook/app/src/main/java/com/example/cmput301w21t23_smartdatabook/favPage.java
@@ -1,4 +1,35 @@
 package com.example.cmput301w21t23_smartdatabook;
 
-public class favPage {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class favPage extends Fragment {
+    public favPage(){
+
+    }
+
+    public static homePage newInstance(String p1, String p2){
+        homePage fragment = new homePage();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.home_page, container, false);
+        // enter code to do shit in the home screen
+        return view;
+    }
 }
--- src/CMPUT301W21T23SmartDataBook/app/src/main/java/com/example/cmput301w21t23_smartdatabook/homePage.java
+++ src/CMPUT301W21T23SmartDataBook/app/src/main/java/com/example/cmput301w21t23_smartdatabook/homePage.java
@@ -1,7 +1,38 @@
 package com.example.cmput301w21t23_smartdatabook;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import androidx.fragment.app.Fragment;
 
 public class homePage extends Fragment {
+    public homePage(){
+
+    }
+
+    public static homePage newInstance(String p1, String p2){
+        homePage fragment = new homePage();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.home_page, container, false);
+        // enter code to do shit in the home screen
+        return view;
+    }
+
+
 
 }
