fdcb1149bfa0cfbfac98d1f91499b259790a0ce8
Alex-Mak-MCW <77552818+Alex-Mak-MCW@users.noreply.github.com>
2021-03-11 21:54:21 -0700

    Update MeasurementTrial.java

--- src/CMPUT301W21T23SmartDataBook/app/src/main/java/com/example/cmput301w21t23_smartdatabook/MeasurementTrial.java
+++ src/CMPUT301W21T23SmartDataBook/app/src/main/java/com/example/cmput301w21t23_smartdatabook/MeasurementTrial.java
@@ -1,5 +1,13 @@
 package com.example.cmput301w21t23_smartdatabook;
 
+/**
+ * Class: Measurement Trial
+ * This class consists the measurement trial, it mainly uses the attributes: input 
+ * It also has getter and setters method for input, in case the experiment's owner need to ignore results
+ * @author Alex Mak 
+ * @see Trial, Experiment
+ */
+
 public class MeasurementTrial extends Trial{
 
     public MeasurementTrial(int count, int numPass, int numFail, float input) {
