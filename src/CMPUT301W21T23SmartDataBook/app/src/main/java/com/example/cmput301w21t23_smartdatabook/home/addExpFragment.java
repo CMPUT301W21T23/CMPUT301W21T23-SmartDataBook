33098f906d63bcb5dc8912557d17f260040d77de
bosco4 <bosco2@ualberta.ca>
2021-03-14 20:50:08 -0600

    New Changes

---
+++ src/CMPUT301W21T23SmartDataBook/app/src/main/java/com/example/cmput301w21t23_smartdatabook/home/addExpFragment.java
@@ -0,0 +1,173 @@
+package com.example.cmput301w21t23_smartdatabook.home;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.NumberPicker;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageButton;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+
+import com.example.cmput301w21t23_smartdatabook.Experiment;
+import com.example.cmput301w21t23_smartdatabook.GetDate;
+import com.example.cmput301w21t23_smartdatabook.R;
+import com.google.android.material.switchmaterial.SwitchMaterial;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Objects;
+
+/**
+ * @Author Afaq
+ * This class is the activity of which contains the add experiment, it has:
+ * An experiment's minimum and maximum number of trials
+ * An experiment's name and description
+ * Radio button to choose which type of the trial the experiment has (binomial/ measurement/count/ non-negtaive count trials)
+ * switch button that turns on/ off an experiment's trial location.
+ * a back button that allows the user to go back
+ * @author Afaq Nabi, Bosco Chan
+ * @see xml files that is associated with this addExpFragment
+ * @version 1
+ */
+public class addExpFragment extends Fragment {
+
+    private FirebaseFirestore db;
+    private FirebaseAuth mAuth;
+
+    private static final int binomialID = 10;
+    private static final int countID = 11;
+    private static final int nonNegativeID = 12;
+    private static final int measurementID = 13;
+
+    private boolean checkLocationOn;
+    private boolean checkPublicOn;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.new_experiment_location_on, container, false);
+
+        ( (AppCompatActivity) getActivity() ).getSupportActionBar().setTitle("Add new experiment");
+
+        NumberPicker maxTrials = view.findViewById(R.id.maxTrialsNumberPicker);
+        NumberPicker minTrials = view.findViewById(R.id.minTrialsNumberPicker);
+
+        TextInputLayout name = view.findViewById(R.id.experimentNameField);
+        TextInputLayout description = view.findViewById(R.id.descriptionField);
+
+        RadioGroup trialChoice = view.findViewById(R.id.typeoFTrialRadioGroup);
+        RadioButton binomial = view.findViewById(R.id.binomialRadioButton);
+        RadioButton count = view.findViewById(R.id.countRadioButton);
+        RadioButton nonNegative = view.findViewById(R.id.nonNegativeRadioButton);
+        RadioButton measurement = view.findViewById(R.id.measurmentRadioButton);
+
+        SwitchMaterial LocationToggle = view.findViewById(R.id.ExperimentLocationToggleSwitch);
+        SwitchMaterial PublicPrivateToggle = view.findViewById(R.id.ExperimentLocationPublicPrivateToggleSwitch);
+
+        //Source: user; https://stackoverflow.com/users/493939/user
+        //Code: https://stackoverflow.com/questions/10356733/getcheckedradiobuttonid-returning-useless-inthttps://stackoverflow.com/questions/10356733/getcheckedradiobuttonid-returning-useless-int
+        binomial.setId(binomialID);
+        count.setId(countID);
+        nonNegative.setId(nonNegativeID);
+        measurement.setId(measurementID);
+
+        //Source: Zoftino; https://www.zoftino.com/
+        //Code: https://www.zoftino.com//android-number-picker-tutorial
+        maxTrials.setMinValue(2);
+        maxTrials.setMaxValue(50);
+        minTrials.setMinValue(1);
+        minTrials.setMaxValue(25);
+
+        LocationToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    checkLocationOn = true;
+                }else{
+                    checkLocationOn = false;
+                }
+            }
+        });
+        PublicPrivateToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    checkPublicOn = true;
+                }else{
+                    checkPublicOn = false;
+                }
+            }
+        });
+
+        final Button addExperiment = view.findViewById(R.id.newExperimentLocationOnCreateButtonView);
+        addExperiment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Instantiate currentDate object to get the current date
+                GetDate currentDate = new GetDate();
+
+                String expName = name.getEditText().getText().toString();
+                String expDescription = description.getEditText().getText().toString();
+                String trialType = findTrialType(trialChoice.getCheckedRadioButtonId());
+                mAuth = FirebaseAuth.getInstance();
+
+                Experiment newExperiment = new Experiment(expName, Objects.requireNonNull(mAuth.getCurrentUser()).getUid(), trialType, expDescription, checkLocationOn, minTrials.getValue(), maxTrials.getValue(), checkPublicOn, currentDate.getFormattedDate());
+
+                //Source: Shweta Chauhan; https://stackoverflow.com/users/6021469/shweta-chauhan
+                //Code: https://stackoverflow.com/questions/40085608/how-to-pass-data-from-one-fragment-to-previous-fragment
+                Intent intent = new Intent(getActivity(), homePage.class);
+                intent.putExtra("newExp", newExperiment);
+                getTargetFragment().onActivityResult(getTargetRequestCode(), 1, intent);
+                getFragmentManager().popBackStack();
+
+            }
+        });
+
+        return view;
+
+    }//onCreateView
+
+    /**
+     * Removes the action bar when the fragment is created, but the removal only exists
+     * after the fragment is destroyed.
+     */
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        ( (AppCompatActivity) getActivity() ).getSupportActionBar().show();
+    }
+
+    /**
+     * Gets the integer value "i" from the RadioGroup and determines what the trial type of the
+     * experiment is based on the given "i" value.
+     * @author Bosco Chan
+     * @param trialTypeID Holds the int value to determine what String the trialType is.
+     * @return trialType
+     */
+    public String findTrialType(int trialTypeID) {
+        switch(trialTypeID) {
+            case binomialID:
+                return "Binomial";
+            case countID:
+                return "Count";
+            case nonNegativeID:
+                return "Non-Negative Count";
+            default:
+                return "Measurement";
+        }
+
+    }//findTrialType
+
+}//addExpFragment
+
+
--- src/CMPUT301W21T23SmartDataBook/app/src/main/java/com/example/cmput301w21t23_smartdatabook/home/homePage.java
+++ src/CMPUT301W21T23SmartDataBook/app/src/main/java/com/example/cmput301w21t23_smartdatabook/home/homePage.java
@@ -19,7 +19,11 @@ import com.example.cmput301w21t23_smartdatabook.CommentActivity;
 import com.example.cmput301w21t23_smartdatabook.Experiment;
 import com.example.cmput301w21t23_smartdatabook.MainActivity;
 import com.example.cmput301w21t23_smartdatabook.R;
+<<<<<<< Updated upstream
 import com.google.android.gms.tasks.OnCompleteListener;
+=======
+import com.example.cmput301w21t23_smartdatabook.experimentDetails;
+>>>>>>> Stashed changes
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
@@ -75,6 +79,12 @@ public class homePage extends Fragment {
                 "Binomial", "testtrial", false, 30,60, true, "03/05/2021"));
         experimentDataList.add(new Experiment("third", "123",
                 "Binomial", "testtrial", false, 30,60, true, "03/05/2021"));
+<<<<<<< Updated upstream
+=======
+        experimentDataList.add(new Experiment("fourth", "123",
+                "Binomial", "testtrial", false, 30,60, true, "03/05/2021"));
+        experimentDataList.add(new Experiment("fifth", "123","Binomial", "testtrial", false, 30,60, true, "03/05/2021"));
+>>>>>>> Stashed changes
 
         experimentAdapter = new CardList(getContext(), experimentDataList);
 
@@ -88,9 +98,97 @@ public class homePage extends Fragment {
             }
         });
 
+<<<<<<< Updated upstream
+=======
+        final FloatingActionButton addExperimentButton = view.findViewById(R.id.add_experiment_button);
+        addExperimentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Source: Shweta Chauhan; https://stackoverflow.com/users/6021469/shweta-chauhan
+                //Code: https://stackoverflow.com/questions/40085608/how-to-pass-data-from-one-fragment-to-previous-fragment
+                addExpFragment addExpFrag = new addExpFragment();
+                addExpFrag.setTargetFragment(homePage.this, 0);
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.container, addExpFrag, "addExpFragment")
+                        .addToBackStack("addExpFragment")
+                        .commit();
+
+            }
+        });
+
+        experimentList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d("test","clicked");
+                Experiment exp = experimentDataList.get(position); // get the experiment from list
+                Intent intent = new Intent(getActivity(), experimentDetails.class);
+                intent.putExtra("position", position); // pass position to experimentDetails class
+                intent.putExtra("experiment", exp); // pass experiment object
+                startActivity(intent);
+            }
+        });
+
+>>>>>>> Stashed changes
         return view;
 
     }//onCreateView
 
+<<<<<<< Updated upstream
+=======
+    @Override
+    public void onResume() {
+        super.onResume();
+//        LoaderManager.getInstance(this).restartLoader(0, null, this);
+
+    }
+
+    //Source: Shweta Chauhan; https://stackoverflow.com/users/6021469/shweta-chauhan
+    //Code: https://stackoverflow.com/questions/40085608/how-to-pass-data-from-one-fragment-to-previous-fragment
+    /**
+     * Custom on activity result function that gets an experiment object from the second fragment
+     * that had been started from this fragment (homePage.java).
+     * @param requestCode Determines which object is wanted from a fragment
+     * @param resultCode Determines what the result is when taken
+     * @param data The intent that holds the serialized object
+     */
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        int addExpFragmentResultCode = 1;
+        int addExpFragmentRequestCode = 0;
+
+        if (resultCode == addExpFragmentResultCode) {
+            if (requestCode == addExpFragmentRequestCode){
+                Experiment newExperiment = (Experiment) data.getSerializableExtra("newExp");
+                Toast.makeText(getActivity(), newExperiment.getExpName() + " " + newExperiment.getDescription() , Toast.LENGTH_SHORT).show();
+                experimentAdapter.add(newExperiment);
+                addExperimentToDB(newExperiment);
+                experimentAdapter.notifyDataSetChanged();
+            }
+        }
+    }//onActivityResult
+
+    /**
+     * Add a new experiment object to the Firebase database.
+     * @param newExperiment The experiment object that is to be added to the Firebase database.
+     */
+    public void addExperimentToDB(Experiment newExperiment) {
+
+        //Add into a Comments collection with a comment document containing
+        //a Pies collection with a pie document
+        db = FirebaseFirestore.getInstance();
+        final CollectionReference allCommentsCollection = db.collection("Experiments");
+        HashMap<String, String> data = new HashMap<>();
+
+        // If there’s some data in the EditText field, then we create a new key-value pair.
+        data.put("Name", newExperiment.getExpName());
+        data.put("Description", newExperiment.getDescription());
+        data.put("Trial Type", newExperiment.getTrialType());
+        data.put("LocationStatus", ""+ newExperiment.getRegionOn());
+        data.put("PublicStatus", ""+ newExperiment.isPublic());
+        data.put("UUID", newExperiment.getOwnerUserID());
+>>>>>>> Stashed changes
 
 }//homePage
