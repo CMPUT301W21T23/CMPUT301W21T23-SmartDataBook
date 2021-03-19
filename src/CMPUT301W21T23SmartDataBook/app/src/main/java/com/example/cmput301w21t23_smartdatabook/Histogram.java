ef57631db68e4c1285acd70fcd47630e2ebcb535
whqkdhfh13 <whqkdhfh13@gmail.com>
2021-03-19 02:58:56 -0600

    Delete everything

--- src/CMPUT301W21T23SmartDataBook/app/src/main/java/com/example/cmput301w21t23_smartdatabook/Histogram.java
+++
@@ -1,9 +0,0 @@
-package com.example.cmput301w21t23_smartdatabook;
-
-public class Histogram extends Experiment{
-    // extends from experiment because experiment has all the trials, subject to change in future if we make a new class to handle such issue
-    public Histogram(String expName, String ownerUserID, String trialType, String description, boolean regionOn, int minTrials, int maxTrials, boolean isPublic, String date) {
-        super(expName, ownerUserID, trialType, description, regionOn, minTrials, maxTrials, isPublic, date);
-    }
-    // will need a xml file for histogram in future
-}
--- src/CMPUT301W21T23SmartDataBook/app/src/main/java/com/example/cmput301w21t23_smartdatabook/trialDialog.java
+++
@@ -1,7 +0,0 @@
-package com.example.cmput301w21t23_smartdatabook;
-
-import androidx.fragment.app.DialogFragment;
-
-public class trialDialog extends DialogFragment {
-
-}
