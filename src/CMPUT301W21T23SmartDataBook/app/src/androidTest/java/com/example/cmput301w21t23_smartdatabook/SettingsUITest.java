e1b6c272658207d1cfa6833f860c89c9e58dfbb5
Afaq <63483862+AfaqNabi@users.noreply.github.com>
2021-03-18 22:34:27 -0600

    update

--- src/CMPUT301W21T23SmartDataBook/app/src/androidTest/java/com/example/cmput301w21t23_smartdatabook/SettingsUITest.java
+++ src/CMPUT301W21T23SmartDataBook/app/src/androidTest/java/com/example/cmput301w21t23_smartdatabook/SettingsUITest.java
@@ -1,4 +1,47 @@
 package com.example.cmput301w21t23_smartdatabook;
 
+import android.app.Activity;
+import android.view.View;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+
+import com.robotium.solo.Solo;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
 public class SettingsUITest {
+
+    private Solo solo;
+    private View addExpButton;
+    private View searchBar;
+
+    @Rule
+    public ActivityTestRule<MainActivity> rule = new ActivityTestRule<MainActivity>(MainActivity.class, true, true);
+
+    @Before
+    public void setUp() {
+        solo = new Solo(InstrumentationRegistry.getInstrumentation(), rule.getActivity());
+        addExpButton = rule.getActivity().findViewById(R.id.add_experiment_button);
+        searchBar = rule.getActivity().findViewById((R.id.app_bar_search));
+    }
+
+    /**
+     * Gets the Activity
+     * @throws Exception
+     */
+    @Test
+    public void start() throws Exception{
+        Activity activity = rule.getActivity();
+    }
+
+    //Finally, add tearDown() method using the @After tag to run after every test method.
+    //This method closes the activity after each test.
+    @After
+    public void tearDown() {
+        solo.finishOpenedActivities();
+    }
 }
