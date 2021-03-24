7e9e19614e1a99c24ff6bc87366a32eaa885bbb1
whqkdhfh13 <whqkdhfh13@gmail.com>
2021-03-23 21:45:11 -0600

    Leaving only location stuff

--- src/CMPUT301W21T23SmartDataBook/app/src/main/java/com/example/cmput301w21t23_smartdatabook/MainActivity.java
+++ src/CMPUT301W21T23SmartDataBook/app/src/main/java/com/example/cmput301w21t23_smartdatabook/MainActivity.java
@@ -27,7 +27,10 @@ public class MainActivity extends AppCompatActivity {
     ArrayAdapter<Experiment> experimentAdapter;
     ArrayList<Experiment> experimentDataList;
 
+    // 1. Define Client that will get a location from Google Play Services
     private FusedLocationProviderClient fusedLocationClient;
+
+    // 2. This function is called when a location is obtained
     // https://stackoverflow.com/questions/44992014/how-to-get-current-location-in-googlemap-using-fusedlocationproviderclient
     LocationCallback mLocationCallback = new LocationCallback() {
         @Override
@@ -42,21 +45,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        experimentList = findViewById(R.id.experimentList);
-        experimentDataList = new ArrayList<>();
-
-        experimentDataList.add(new Experiment("first", "123",
-                "Binomial", "testtrial", false, 30, 60, true, "03/05/2021"));
-        experimentDataList.add(new Experiment("second", "123",
-                "Binomial", "testtrial", false, 30, 60, true, "03/05/2021"));
-        experimentDataList.add(new Experiment("third", "123",
-                "Binomial", "testtrial", false, 30, 60, true, "03/05/2021"));
-
-        experimentAdapter = new CardList(this, experimentDataList);
-
-        experimentList.setAdapter(experimentAdapter);
-
-
+        // 3. Initialize the client
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
         final Button location_button = findViewById(R.id.location_test);
@@ -64,29 +53,13 @@ public class MainActivity extends AppCompatActivity {
         location_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                // 4. This is for getting a location. It first checks for the permission and if permission is not granted, it requests for the permission.
+                //    Else, the client requests for a last known location and onLocationResult() runs from mLocationCallback.
                 if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
                     ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 78);
                 } else {
                     fusedLocationClient.requestLocationUpdates(LocationRequest.create().setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY), mLocationCallback, Looper.myLooper());
-                    // Below method executes when location is successfully obtained, deprecated due to above mLocationCallback
-//                    fusedLocationClient.getLastLocation().addOnSuccessListener(MainActivity.this, new OnSuccessListener<Location>() {
-//                        @Override
-//                        public void onSuccess(Location location) {
-//                            // Got last known location. In some rare situations this can be null.
-//                            if (location != null) {
-//                                System.out.println(location.toString());
-//                            } else {
-//                                System.out.println("Location do not exist yet. Maybe something wrong?");
-//                            }
-//                        }
-//                    });
                 }
             }
         });
