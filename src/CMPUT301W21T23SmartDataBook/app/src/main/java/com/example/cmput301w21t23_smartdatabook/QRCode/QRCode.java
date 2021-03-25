82cf870c233cfbcc4002234d17139e895c38bc38
Afaq <63483862+AfaqNabi@users.noreply.github.com>
2021-03-25 06:32:58 -0600

    Update QRCode.java

--- src/CMPUT301W21T23SmartDataBook/app/src/main/java/com/example/cmput301w21t23_smartdatabook/QRCode/QRCode.java
+++ src/CMPUT301W21T23SmartDataBook/app/src/main/java/com/example/cmput301w21t23_smartdatabook/QRCode/QRCode.java
@@ -11,28 +11,50 @@ import com.google.zxing.qrcode.QRCodeWriter;
 // https://github.com/kenglxn/QRGen
 // https://stackoverflow.com/questions/8800919/how-to-generate-a-qr-code-for-an-android-application
 public class QRCode {
-//    QRCodeWriter writer = new QRCodeWriter();
-//    Bitmap myBitmap = QRCode.from("www.example.org").bitmap();
+    QRCodeWriter writer = new QRCodeWriter();
+//    Bitmap myBitmap = QRCode.from("www").bitmap();
+
+    public void generate(){
+        String content= "m";
+        BitMatrix bitMatrix = null;
+        try {
+            bitMatrix = writer.encode(content, BarcodeFormat.QR_CODE, 512, 512);
+        } catch (WriterException writerException) {
+            writerException.printStackTrace();
+        }
+        int width = bitMatrix.getWidth();
+        int height = bitMatrix.getHeight();
+        Bitmap bmp = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                bmp.setPixel(x, y, bitMatrix.get(x, y) ? Color.BLACK : Color.WHITE);
+            }
+        }
+//        ((ImageView) findViewById(R.id.img_result_qr)).setImageBitmap(bmp);
+
+    }
 
 //    try {
-//        String content= "m";
-//        BitMatrix bitMatrix = null;
-//        try {
-//            bitMatrix = writer.encode(content, BarcodeFormat.QR_CODE, 512, 512);
-//        } catch (WriterException writerException) {
-//            writerException.printStackTrace();
-//        }
-//        int width = bitMatrix.getWidth();
-//        int height = bitMatrix.getHeight();
-//        Bitmap bmp = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
-//        for (int x = 0; x < width; x++) {
-//            for (int y = 0; y < height; y++) {
-//                bmp.setPixel(x, y, bitMatrix.get(x, y) ? Color.BLACK : Color.WHITE);
+//            String content= "m";
+//            BitMatrix bitMatrix = null;
+//            try {
+//                bitMatrix = writer.encode(content, BarcodeFormat.QR_CODE, 512, 512);
+//            } catch (WriterException writerException) {
+//                writerException.printStackTrace();
 //            }
-//        }
+//            int width = bitMatrix.getWidth();
+//            int height = bitMatrix.getHeight();
+//            Bitmap bmp = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+//            for (int x = 0; x < width; x++) {
+//                for (int y = 0; y < height; y++) {
+//                    bmp.setPixel(x, y, bitMatrix.get(x, y) ? Color.BLACK : Color.WHITE);
+//                }
+//            }
 ////        ((ImageView) findViewById(R.id.img_result_qr)).setImageBitmap(bmp);
 //
-//    } catch(WriterException e) {
-//        e.printStackTrace();
-//    }
+//        } catch(WriterException e) {
+//            e.printStackTrace();
+//        }
+
+
 }
