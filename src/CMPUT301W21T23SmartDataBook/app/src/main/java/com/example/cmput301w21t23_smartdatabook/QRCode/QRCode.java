10e98d5f41471b6ffd297fef956e8b6b3727627f
Afaq <63483862+AfaqNabi@users.noreply.github.com>
2021-03-26 22:26:13 -0600

    update

--- src/CMPUT301W21T23SmartDataBook/app/src/main/java/com/example/cmput301w21t23_smartdatabook/QRCode/QRCode.java
+++ src/CMPUT301W21T23SmartDataBook/app/src/main/java/com/example/cmput301w21t23_smartdatabook/QRCode/QRCode.java
@@ -5,9 +5,6 @@ import android.graphics.Color;
 
 import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcode;
 import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetectorOptions;
-import com.google.mlkit.vision.barcode.Barcode;
-import com.google.mlkit.vision.barcode.BarcodeScannerOptions;
-import com.google.mlkit.vision.common.InputImage;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
@@ -35,42 +32,7 @@ public class QRCode {
                 bmp.setPixel(x, y, bitMatrix.get(x, y) ? Color.BLACK : Color.WHITE);
             }
         }
-//        ((ImageView) findViewById(R.id.img_result_qr)).setImageBitmap(bmp);
         return bmp;
     }
-
-    public void qrCodeScanner(){
-        BarcodeScannerOptions options =
-                new BarcodeScannerOptions.Builder()
-                        .setBarcodeFormats(
-                                Barcode.FORMAT_QR_CODE,
-                                Barcode.FORMAT_AZTEC,
-                                Barcode.FORMAT_CODABAR)
-                        .build();
-        InputImage image = InputImage.fromBitmap(bitmap, rotationDegree);
-    }
-
-//    try {
-//            String content= "m";
-//            BitMatrix bitMatrix = null;
-//            try {
-//                bitMatrix = writer.encode(content, BarcodeFormat.QR_CODE, 512, 512);
-//            } catch (WriterException writerException) {
-//                writerException.printStackTrace();
-//            }
-//            int width = bitMatrix.getWidth();
-//            int height = bitMatrix.getHeight();
-//            Bitmap bmp = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
-//            for (int x = 0; x < width; x++) {
-//                for (int y = 0; y < height; y++) {
-//                    bmp.setPixel(x, y, bitMatrix.get(x, y) ? Color.BLACK : Color.WHITE);
-//                }
-//            }
-////        ((ImageView) findViewById(R.id.img_result_qr)).setImageBitmap(bmp);
-//
-//        } catch(WriterException e) {
-//            e.printStackTrace();
-//        }
-
 
 }
--- src/CMPUT301W21T23SmartDataBook/app/src/main/java/com/example/cmput301w21t23_smartdatabook/QRCode/ScanQrActivity.java
+++ src/CMPUT301W21T23SmartDataBook/app/src/main/java/com/example/cmput301w21t23_smartdatabook/QRCode/ScanQrActivity.java
@@ -1,13 +1,81 @@
 package com.example.cmput301w21t23_smartdatabook.QRCode;
 
+import android.Manifest;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class ScanQrActivity extends AppCompatActivity {
+import com.example.cmput301w21t23_smartdatabook.R;
+import com.example.cmput301w21t23_smartdatabook.database.Database;
+import com.example.cmput301w21t23_smartdatabook.mainController.MainActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.zxing.Result;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class ScanQrActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler{
+	ZXingScannerView scannerView;
+	Database database = new Database();
+	FirebaseFirestore db = FirebaseFirestore.getInstance();
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		scannerView=new ZXingScannerView(this);
+		setContentView(scannerView);
+		Dexter.withContext(getApplicationContext())
+				.withPermission(Manifest.permission.CAMERA)
+				.withListener(new PermissionListener() {
+					@Override
+					public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+						scannerView.startCamera();
+					}
+
+					@Override
+					public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+
+					}
+
+					@Override
+					public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+						permissionToken.continuePermissionRequest();
+					}
+				}).check();
+	}
+
+	@Override
+	public void handleResult(Result rawResult) {
+//		String data=rawResult.getText().toString();
+
+//		dbref.push().setValue(data)
+//				.addOnCompleteListener(new OnCompleteListener<Void>() {
+//					@Override
+//					public void onComplete(@NonNull Task<Void> task) {
+//						MainActivity.qrtext.setText("Data inserted Successfully");
+//						onBackPressed();
+//					}
+//				});
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		scannerView.stopCamera();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		scannerView.setResultHandler(this);
+		scannerView.startCamera();
 	}
 }
 
