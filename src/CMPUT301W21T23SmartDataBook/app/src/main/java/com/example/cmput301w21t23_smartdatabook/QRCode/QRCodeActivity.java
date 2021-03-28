b1f4b91898c0c9ad783913962309b5984852edf8
Afaq <63483862+AfaqNabi@users.noreply.github.com>
2021-03-27 20:52:14 -0600

    update qr code generator

--- src/CMPUT301W21T23SmartDataBook/app/src/main/java/com/example/cmput301w21t23_smartdatabook/QRCode/QRCodeActivity.java
+++ src/CMPUT301W21T23SmartDataBook/app/src/main/java/com/example/cmput301w21t23_smartdatabook/QRCode/QRCodeActivity.java
@@ -5,9 +5,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Switch;
+import android.widget.TextView;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -39,9 +41,27 @@ public class QRCodeActivity extends AppCompatActivity {
 
         EditText value = findViewById(R.id.passesEditTextBinomialQR);
         Switch toggle = findViewById(R.id.passFail);
+        TextView switchBtn = findViewById(R.id.true_false);
+
+
+        toggle.setTextOn("True");
+        toggle.setTextOff("False");
+
+        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    switchBtn.setText("True");
+                }
+                else {
+                    switchBtn.setText("False");
+                }
+            }
+        });
 
         if(experiment.getTrialType().equals("Binomial")){
             toggle.setVisibility(View.VISIBLE);
+            switchBtn.setVisibility(View.VISIBLE);
         }
 
         CheckBox location = findViewById(R.id.binomialTrialLocationCheckBox);
@@ -54,14 +74,20 @@ public class QRCodeActivity extends AppCompatActivity {
                 String QRCodeMessage = experiment.getExpID()+","+user.getUserUniqueID()+","+value.getText().toString();
 
                 value.setText("");
+                if (experiment.getTrialType().equals("Binomial")){
+                    if (toggle.isChecked()){
+                        QRCodeMessage+=",true";
+                    }
+                    else{
+                        QRCodeMessage+=",false";
+                    }
+                }
 
                 ImageView QRCode = findViewById(R.id.binomialReplaceImageQrCode);
 
                 QRCode.setImageBitmap(QRcode.generate(QRCodeMessage));
             }
         });
-
-
 
     }
 }
