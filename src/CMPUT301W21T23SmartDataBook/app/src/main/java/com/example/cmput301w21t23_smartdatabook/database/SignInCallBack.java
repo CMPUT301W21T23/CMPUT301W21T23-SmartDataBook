09fa66338f31bdf4f26ff893461e5c231a8b38e7
whqkdhfh13 <whqkdhfh13@gmail.com>
2021-03-27 23:28:20 -0600

    Delete signInCallBack

--- src/CMPUT301W21T23SmartDataBook/app/src/main/java/com/example/cmput301w21t23_smartdatabook/database/SignInCallBack.java
+++
@@ -1,6 +0,0 @@
-package com.example.cmput301w21t23_smartdatabook.database;
-
-//Handles the coordination of updating mainActivity with homepage
-public interface SignInCallBack {
-    void updateHomeScreen(String userID);
-}
