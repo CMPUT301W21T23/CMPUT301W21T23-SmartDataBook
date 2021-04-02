23789f87b6ddd215b9389631ad79f50e7cf1dae7
Afaq <63483862+AfaqNabi@users.noreply.github.com>
2021-04-02 16:15:45 -0600

    g

--- src/CMPUT301W21T23SmartDataBook/app/src/main/java/com/example/cmput301w21t23_smartdatabook/experimentDetails/ExperimentDetails.java
+++ src/CMPUT301W21T23SmartDataBook/app/src/main/java/com/example/cmput301w21t23_smartdatabook/experimentDetails/ExperimentDetails.java
@@ -14,6 +14,7 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageButton;
 
 import com.example.cmput301w21t23_smartdatabook.QRCode.QRCodeActivity;
 import com.example.cmput301w21t23_smartdatabook.QRCode.ScannerActivity;
@@ -61,7 +62,7 @@ public class ExperimentDetails extends AppCompatActivity {
         View userInfoView = LayoutInflater.from(ExperimentDetails.this).inflate(R.layout.view_profile, null);
 
 
-        Button scan = findViewById(R.id.scanner);
+        AppCompatImageButton scan = findViewById(R.id.scanner);
         scan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
