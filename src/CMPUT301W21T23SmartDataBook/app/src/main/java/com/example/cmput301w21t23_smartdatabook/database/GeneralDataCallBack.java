a0a4fbb34f07991bcb352024e74fece08c89a391
Alex-Mak-MCW <77552818+Alex-Mak-MCW@users.noreply.github.com>
2021-04-04 20:34:12 -0600

    added brief javadocs to all files in the database folder

--- src/CMPUT301W21T23SmartDataBook/app/src/main/java/com/example/cmput301w21t23_smartdatabook/database/GeneralDataCallBack.java
+++ src/CMPUT301W21T23SmartDataBook/app/src/main/java/com/example/cmput301w21t23_smartdatabook/database/GeneralDataCallBack.java
@@ -1,7 +1,8 @@
 package com.example.cmput301w21t23_smartdatabook.database;
 
 /**
- * Callback interface that handles the general data, so it passes the data to someone, and let other users to call it in future
+ * Callback interface that handles the general data, so it passes the data from 1 class to other class, and let other classes to call it in future
+ * Synchronous will ask data from asynchronous, callback helps and returns data back to synchronous
  * @author Bosco Chan
  */
 public interface GeneralDataCallBack {
