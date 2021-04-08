a073de47423985689a807c62532ef50a18258f78
Afaq <63483862+AfaqNabi@users.noreply.github.com>
2021-04-08 14:12:26 -0600

    update

--- src/CMPUT301W21T23SmartDataBook/app/src/test/java/com/example/cmput301w21t23_smartdatabook/StatisticsModelTest.java
+++ src/CMPUT301W21T23SmartDataBook/app/src/test/java/com/example/cmput301w21t23_smartdatabook/StatisticsModelTest.java
@@ -1,5 +1,14 @@
 package com.example.cmput301w21t23_smartdatabook;
 
+import com.example.cmput301w21t23_smartdatabook.stats.StatisticsModel;
+
+import java.util.ArrayList;
+
 public class StatisticsModelTest {
+
+    private void mockObject(){
+        ArrayList<ArrayList> mockArray = new ArrayList<>();
+        ArrayList<Object> mockSubArray = new ArrayList<>();
+    }
 
 }
