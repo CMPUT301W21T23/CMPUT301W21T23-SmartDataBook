413b761238ec70eeb2477367933235f4273ec5ae
bosco4 <bosco2@ualberta.ca>
2021-04-08 16:30:37 -0600

    removed if statement inside list population

--- src/CMPUT301W21T23SmartDataBook/app/src/main/java/com/example/cmput301w21t23_smartdatabook/home/HomePage.java
+++ src/CMPUT301W21T23SmartDataBook/app/src/main/java/com/example/cmput301w21t23_smartdatabook/home/HomePage.java
@@ -152,6 +152,7 @@ public class HomePage extends Fragment {
 			public void onScrollStateChanged(AbsListView view, int scrollState) {
 				if (scrollState == AbsListView.OnScrollListener.SCROLL_STATE_IDLE) {
 					experimentList.invalidateViews();
+
 				}
 			}
 
@@ -181,23 +182,6 @@ public class HomePage extends Fragment {
 						//experimentDataList with added items ONLY exist inside the scope of this getExpDataList function
 						experimentDataList = DataList;
 
-//						//Create a new searchDataList depending on the query
-//						if (currentQuery != null) {
-//							for (Experiment experiment : experimentDataList) {
-//								if (experiment.getExpName().contains(currentQuery) ||
-//										UserName.get(experiment.getOwnerUserID()).getUserName().contains(currentQuery) ||
-//										experiment.getDate().toString().contains(currentQuery) ||
-//										experiment.getDescription().contains(currentQuery)) {
-//
-//									searchDataList.add(experiment);
-//
-//								}
-//							}
-//							experimentAdapter.clear();
-//							experimentAdapter.addAll(searchDataList);
-//						} else {
-//							experimentAdapter.addAll(experimentDataList);
-//						}
 
 						experimentAdapter.addAll(experimentDataList);
 						experimentAdapter.notifyDataSetChanged();
